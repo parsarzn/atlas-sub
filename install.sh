25bdfc5ff3b451965d290908318d3474eafe7ad2
parsarzn <parsa.rezaian@gmail.com>
2025-07-31 02:48:59 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,14 @@
 sudo wget -N -P /var/lib/marzban/templates/subscription/ https://raw.githubusercontent.com/parsarzn/atlas-sub/main/index.html
 
 ENV_FILE="/opt/marzban/.env"
-LINE1_PATTERN='^CUSTOM_TEMPLATES_DIRECTORY\s*=\s*"/var/lib/marzban/templates/"$'
-LINE2_PATTERN='^SUBSCRIPTION_PAGE_TEMPLATE\s*=\s*"subscription/index.html"$'
 TO_ADD=""
 
-if ! tr -d '[:space:]' < "$ENV_FILE" | grep -q 'CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzban/templates/"'; then
+# بررسی وجود خطوط فقط در خطوطی که با # شروع نمی‌شن (یعنی فعال هستن)
+if ! grep -E '^\s*[^#]*CUSTOM_TEMPLATES_DIRECTORY\s*=\s*"/var/lib/marzban/templates/"' "$ENV_FILE" > /dev/null; then
     TO_ADD+='CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzban/templates/"'$'\n'
 fi
 
-if ! tr -d '[:space:]' < "$ENV_FILE" | grep -q 'SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"'; then
+if ! grep -E '^\s*[^#]*SUBSCRIPTION_PAGE_TEMPLATE\s*=\s*"subscription/index.html"' "$ENV_FILE" > /dev/null; then
     TO_ADD+='SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"'$'\n'
 fi
 
