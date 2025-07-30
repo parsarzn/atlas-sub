59e8daa716b63f8e468648a193753fa2b24ac016
parsarzn <parsa.rezaian@gmail.com>
2025-07-31 02:37:36 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 sudo wget -N -P /var/lib/marzban/templates/subscription/ https://raw.githubusercontent.com/parsarzn/atlas-sub/main/index.html
 
 ENV_FILE="/opt/marzban/.env"
-LINE1='CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzban/templates/"'
-LINE2='SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"'
+LINE1_PATTERN='^CUSTOM_TEMPLATES_DIRECTORY\s*=\s*"/var/lib/marzban/templates/"$'
+LINE2_PATTERN='^SUBSCRIPTION_PAGE_TEMPLATE\s*=\s*"subscription/index.html"$'
 TO_ADD=""
 
-if ! grep -Fxq "$LINE1" "$ENV_FILE"; then
-    TO_ADD+="$LINE1"$'\n'
+if ! grep -Pq "$LINE1_PATTERN" "$ENV_FILE"; then
+    TO_ADD+='CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzban/templates/"'$'\n'
 fi
 
-if ! grep -Fxq "$LINE2" "$ENV_FILE"; then
-    TO_ADD+="$LINE2"$'\n'
+if ! grep -Pq "$LINE2_PATTERN" "$ENV_FILE"; then
+    TO_ADD+='SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"'$'\n'
 fi
 
 if [ -n "$TO_ADD" ]; then
